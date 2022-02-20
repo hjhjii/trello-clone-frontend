cceacf569efe37b3b03728de3ba246a5f8c81391
Monkey <gigghi05@gmail.com>
2022-02-20 23:13:28 +0100

    new behavior for addItem textarea

--- src/ToDoCard.js
+++ src/ToDoCard.js
@@ -3,6 +3,21 @@ import { useState } from 'react'
 
 function ToDoCard(props) {
     const [isAddActionActive, setAddActionActive] = useState(false)
+    const [text, setText] = useState("")
+    const textAreaFragment = <>
+        <textarea className="Item" onBlur={(event) => {
+            setAddActionActive(!isAddActionActive)
+            setText(event.target.value)
+        }} autoFocus></textarea>
+        <div autoFocus>
+            <button className="Item Item-button">X</button>
+            <button className="Item Item-button">X</button>
+        </div>
+    </>
+    const addItemButtonFragment = <>
+        <button className="Item Item-button add-item-button" onClick={() => setAddActionActive(!isAddActionActive)}>Add a item</button>
+    </>
+
     return <>
         <div className="ToDoCard">
             <div className="ToDoCard-header">
@@ -10,14 +25,11 @@ function ToDoCard(props) {
             </div>
             <div className="ToDoCard-body">
                 <Item text={"texttexttexttexttexttexttexttexttexttexttexttexttexttexttext"} />
-                <div>
-                    <div className="container-add-item">
-
-                        {/* <button className="Item Item-button">X</button> */}
-                        {isAddActionActive ? <textarea className="Item"></textarea> : <button className="Item Item-button add-item-button" onClick={() => setAddActionActive(!isAddActionActive)}>Add a item</button>}
-                    </div>
+                <div className="container-add-item" style={isAddActionActive ? { flexDirection: "column" } : {}}>
+                    {isAddActionActive ? textAreaFragment : addItemButtonFragment}
                 </div>
             </div>
+            {text ? <h2>{text}</h2> : ""}
         </div>
     </>
 }
